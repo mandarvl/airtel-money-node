16c87bbe6ef9d56807936aef6f7e69195b1a97ea
mandarvl <manda.ravalison@gmail.com>
2023-10-13 13:38:52 +0300

    make generate token example

--- examples/generate-token.ts
+++ examples/generate-token.ts
@@ -3,8 +3,8 @@ import { AirtelMoney, STAGING_URL } from "../src";
 async function main() {
   const clientId = process.env.CLIENT_ID;
   const clientSecret = process.env.CLIENT_SECRET;
-  const mvola = new AirtelMoney(STAGING_URL);
-  const data = await mvola.auth.generateToken(clientId!, clientSecret!);
+  const airtel = new AirtelMoney(STAGING_URL);
+  const data = await airtel.auth.generateToken(clientId!, clientSecret!);
   console.log(data);
 }
 
